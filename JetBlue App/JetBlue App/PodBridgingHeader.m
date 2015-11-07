69ecad92c310309521cf63975acfd4d900089d3c
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-11-07 16:49:31 -0500

    implemented swipes from Gao Chao's repository

---
+++ JetBlue App/JetBlue App/PodBridgingHeader.m
@@ -0,0 +1,16 @@
+//
+//  PodBridgingHeader.m
+//  JetBlue App
+//
+//  Created by Cameron Averill on 11/7/15.
+//  Copyright (c) 2015 Cameron Averill. All rights reserved.
+//
+
+//create a bridging header to use obj c code from MPCSwipeToChoose pod
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <UIKit/UIKit.h>
+#import <MDCSwipeToChoose/MDCSwipeToChoose.h>
+
+#endif
--- JetBlue App/JetBlue App/ViewController.swift
+++ JetBlue App/JetBlue App/ViewController.swift
@@ -8,32 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController{
     
-    var tableData = []
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
-        return 10
-    }
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
-        
-        cell.textLabel?.text = "Row #\(indexPath.row)"
-        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
-        
-        return cell
-    }
-
-    @IBOutlet weak var AppsTable: UITableView!
+//    var tableData = []
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
+//        return 10
+//    }
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+//        
+//        cell.textLabel?.text = "Row #\(indexPath.row)"
+//        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+//        
+//        return cell
+//    }
+//
+//    @IBOutlet weak var AppsTable: UITableView!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+    // ... in a view controller
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var options = MDCSwipeToChooseViewOptions()
+        options.delegate = self
+        options.likedText = "Keep"
+        options.likedColor = UIColor.blueColor()
+        options.nopeText = "Delete"
+        options.onPan = { state -> Void in
+            if state.thresholdRatio == 1 && state.direction == MDCSwipeDirection.Left {
+                println("Photo deleted!")
+            }
+        }
+        
+        var view = MDCSwipeToChooseView(frame: self.view.bounds, options: options)
+        view.imageView.image = UIImage(named: "photo.png")
+        self.view.addSubview(view)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    // This is called when a user didn't fully swipe left or right.
+    func viewDidCancelSwipe(view: UIView) -> Void{
+        println("Couldn't decide, huh?")
+    }
+    
+    // Sent before a choice is made. Cancel the choice by returning `false`. Otherwise return `true`.
+    func view(view: UIView, shouldBeChosenWithDirection: MDCSwipeDirection) -> Bool{
+        if (shouldBeChosenWithDirection == MDCSwipeDirection.Left) {
+            return true;
+        } else {
+            // Snap the view back and cancel the choice.
+            UIView.animateWithDuration(0.16, animations: { () -> Void in
+                view.transform = CGAffineTransformIdentity
+                view.center = view.superview!.center
+            })
+            return false;
+        }
+    }
+    
+    // This is called then a user swipes the view fully left or right.
+    func view(view: UIView, wasChosenWithDirection: MDCSwipeDirection) -> Void{
+        if wasChosenWithDirection == MDCSwipeDirection.Left {
+            println("Photo deleted!")
+        }else{
+            println("Photo saved!")
+        }
     }
 
 
