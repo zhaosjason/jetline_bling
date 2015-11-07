82a1716ac9ee0184048fce76e26d037361d13844
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-11-07 11:27:01 -0500

    created repository for ios

---
+++ JetBlue App/JetBlue App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JetBlue App
+//
+//  Created by Cameron Averill on 11/7/15.
+//  Copyright (c) 2015 Cameron Averill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
