55081a1c01f0a4d63889d0b97a948986c013889b
Jason Zhao <zhao473@purdue.edu>
2015-11-07 22:59:09 -0500

    Updated api interface

--- server/collectionDriver.js
+++ server/collectionDriver.js
@@ -1,79 +1,86 @@
 var ObjectID = require('mongodb').ObjectID;
 
 CollectionDriver = function(db) {
-  this.db = db;
+	this.db = db;
 };
 
 CollectionDriver.prototype.getCollection = function(collectionName, callback) {
-  this.db.collection(collectionName, function(error, the_collection) {
-    if( error ) callback(error);
-    else callback(null, the_collection);
-  });
+	this.db.collection(collectionName, function(error, the_collection) {
+		if( error ) callback(error);
+		else callback(null, the_collection);
+	});
 };
+
 
 CollectionDriver.prototype.findAll = function(collectionName, callback) {
-    this.getCollection(collectionName, function(error, the_collection) { //A
-      if( error ) callback(error);
-      else {
-        the_collection.find().toArray(function(error, results) { //B
-          if( error ) callback(error);
-          else callback(null, results);
-        });
-      }
-    });
+	this.getCollection(collectionName, function(error, the_collection) {
+	  if( error ) callback(error);
+	  else {
+		the_collection.find().toArray(function(error, results) {
+		  if( error ) callback(error);
+		  else callback(null, results);
+		});
+	  }
+	});
 };
 
-CollectionDriver.prototype.get = function(collectionName, loc, callback) { //A
-    this.getCollection(collectionName, function(error, the_collection) {
-        if (error) callback(error);
-        else {
-            the_collection.distinct('Hotel Property', {'Origin':loc}, function(error,doc) { //C
-              if (error) callback(error);
-              else callback(null, doc);
-            });
-        }
-    });
+CollectionDriver.prototype.get = function(collectionName, cmd, q, callback) {
+	this.getCollection(collectionName, function(error, the_collection) {
+		if (error) callback(error);
+		else {
+			if(cmd == 'loc'){
+				the_collection.distinct('Hotel Property', {'Origin':q}, function(error,doc) {
+					if (error) callback(error);
+					else callback(null, doc);
+				});
+			}
+			else if(cmd == 'friends'){
+				the_collection.find({},{'Location':q}).toArray(function(error,doc) {
+					if (error) callback(error);
+					else callback(null, doc);
+				});
+			}
+		}
+	});
 };
 
-//save new object
-CollectionDriver.prototype.save = function(collectionName, obj, callback) {
-    this.getCollection(collectionName, function(error, the_collection) { //A
-      if( error ) callback(error)
-      else {
-        obj.created_at = new Date(); //B
-        the_collection.insert(obj, function() { //C
-          callback(null, obj);
-        });
-      }
-    });
+// Add liked trips to user model
+CollectionDriver.prototype.save = function(collectionName, obj, entityId, callback) {
+	this.getCollection(collectionName, function(error, the_collection) { 
+		if (error) callback(error);
+		else {
+			the_collection.update(fbid:'entityId', {$push: {likes: obj._id}}, function() { 
+				callback(null, obj);
+			});
+		}
+	});
 };
 
-//update a specific object
+// Update facebook friends list in users collection
 CollectionDriver.prototype.update = function(collectionName, obj, entityId, callback) {
-    this.getCollection(collectionName, function(error, the_collection) {
-        if (error) callback(error);
-        else {
-            obj._id = ObjectID(entityId); //A convert to a real obj id
-            obj.updated_at = new Date(); //B
-            the_collection.save(obj, function(error,doc) { //C
-                if (error) callback(error);
-                else callback(null, obj);
-            });
-        }
-    });
+	this.getCollection(collectionName, function(error, the_collection) {
+		if (error) callback(error);
+		else {
+			the_collection.update({fbid: entityId}, {$set: {friends: obj.friends}}, function(error,doc) {
+				if (error) callback(error);
+				else callback(null, obj);
+			});
+		}
+	});
 };
 
 //delete a specific object
+/*
 CollectionDriver.prototype.delete = function(collectionName, entityId, callback) {
-    this.getCollection(collectionName, function(error, the_collection) { //A
-        if (error) callback(error);
-        else {
-            the_collection.remove({'_id':ObjectID(entityId)}, function(error,doc) { //B
-                if (error) callback(error);
-                else callback(null, doc);
-            });
-        }
-    });
-};
+	this.getCollection(collectionName, function(error, the_collection) { //A
+		if (error) callback(error);
+		else {
+			the_collection.remove({'_id':ObjectID(entityId)}, function(error,doc) { //B
+				if (error) callback(error);
+				else callback(null, doc);
+			});
+		}
+	});
+};*/
 
 exports.CollectionDriver = CollectionDriver;
--- server/index.js
+++ server/index.js
@@ -4,96 +4,81 @@ CollectionDriver = require('./collectionDriver').CollectionDriver;
 
 var app = express();
 app.set('port', process.env.PORT || 3000); 
-app.set('views', path.join(__dirname, 'views')); //A
-app.set('view engine', 'jade'); //B
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'jade'); 
 
 app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Begin Mongo stuff
-var mongoHost = 'localhost'; //A
+var mongoHost = 'localhost'; 
 var mongoPort = 27017; 
 var collectionDriver;
  
-var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
-mongoClient.open(function(err, mongoClient) { //C
-  if (!mongoClient) {
-      console.error("Error! Exiting... Must start MongoDB first");
-      process.exit(1); //D
-  }
-  var db = mongoClient.db("mongodb");  //E
-  collectionDriver = new CollectionDriver(db); //F
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); 
+mongoClient.open(function(err, mongoClient) { 
+	if (!mongoClient) {
+		console.error("Error! Exiting... Must start MongoDB first");
+		process.exit(1); 
+	}
+	var db = mongoClient.db("mongodb");  
+	collectionDriver = new CollectionDriver(db); 
 });
  
-// Mongo routes
-app.get('/:collection', function(req, res) { //A
-   var params = req.params; //B
-   collectionDriver.findAll(req.params.collection, function(error, objs) { //C
-    	  if (error) { res.send(400, error); } //D
-	      else { 
-	          if (req.accepts('html')) { //E
-    	          res.render('data',{objects: objs, collection: req.params.collection}); //F
-              } else {
-	          res.set('Content-Type','application/json'); //G
-                  res.send(200, objs); //H
-              }
-         }
-   	});
+/**
+ *  Routes
+ */
+app.get('/:collection', function(req, res) { // GET entire database
+	var params = req.params;
+	collectionDriver.findAll(req.params.collection, function(error, objs) {
+		if (error) { res.send(400, error); }
+		else { 
+			if (req.accepts('html')) {
+				res.render('data',{objects: objs, collection: req.params.collection});
+			} else {
+			res.set('Content-Type','application/json');
+				res.send(200, objs);
+			}
+		}
+	});
 });
  
-app.get('/:collection/:entity', function(req, res) { //I
-   var params = req.params;
-   var entity = params.entity;
-   var collection = params.collection;
-   if (entity) {
-       collectionDriver.get(collection, entity, function(error, objs) { //J
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //K
-       });
-   } else {
-      res.send(400, {error: 'bad url', url: req.url});
-   }
+app.get('/:collection/:entity/', function(req, res) { // GET more cards
+	var params = req.params;
+	var entity = params.entity;
+	var collection = params.collection;
+	if (entity) {
+		collectionDriver.get(collection, entity, function(error, objs) {
+			if (error) { res.send(400, error); }
+			else { res.send(200, objs); }
+		});
+	} else {
+		res.send(400, {error: 'bad url', url: req.url});
+	}
 });
 
-app.post('/:collection', function(req, res) { //A
-    var object = req.body;
-    var collection = req.params.collection;
-    collectionDriver.save(collection, object, function(err,docs) {
-          if (err) { res.send(400, err); } 
-          else { res.send(201, docs); } //B
-     });
+app.post('/:collection/:entity', function(req, res) { // POST liked trips
+	var params = req.params;
+	var collection = params.collection;
+	var entity = params.entity;
+	collectionDriver.save(collection, req.body, entity, function(err,docs) {
+		if (err) { res.send(400, err); } 
+		else { res.send(201, docs); } 
+	});
 });
 
-app.put('/:collection/:entity', function(req, res) { //A
-    var params = req.params;
-    var entity = params.entity;
-    var collection = params.collection;
-    if (entity) {
-       collectionDriver.update(collection, req.body, entity, function(error, objs) { //B
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //C
-       });
-   } else {
-       var error = { "message" : "Cannot PUT a whole collection" };
-       res.send(400, error);
-   }
+app.put('/:collection/:entity', function(req, res) { // PUT updated location and friend list
+	var params = req.params;
+	var collection = params.collection;
+	var entity = params.entity;
+	collectionDriver.update(collection, req.body, entity, function(error, objs) { 
+		if (error) { res.send(400, error); }
+		else { res.send(200, objs); } 
+	});
 });
-
-app.delete('/:collection/:entity', function(req, res) { //A
-    var params = req.params;
-    var entity = params.entity;
-    var collection = params.collection;
-    if (entity) {
-       collectionDriver.delete(collection, entity, function(error, objs) { //B
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //C 200 b/c includes the original doc
-       });
-   } else {
-       var error = { "message" : "Cannot DELETE a whole collection" };
-       res.send(400, error);
-   }
-});
-// End Mongo stuff
+/**
+ *  End routes
+ */
  
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
